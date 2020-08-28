8f537453d5ed224ec75d5e1b10881701ef032c4c
Andy <kas2850.cuk@yandex.ru>
2020-08-28 22:10:05 +0300

    correction linter

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,16 +1,6 @@
-"""
-Importing the library prompt.
-
-Ask for a name and say 'Hello'.
-"""
 import prompt
 
 
 def welcome_user():
-    """
-    Get the user name.
-
-    Say 'Hello' to the user.
-    """
     name = prompt.string('\nMay I have your name? ')
     print('Hello, {0}!'.format(name))
